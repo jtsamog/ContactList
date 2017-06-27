27489bdb7b1f433b94cd4166dc4b3cbdc2295665
Sam Ogunyowa <mailsamog@gmail.com>
2017-06-27 17:29:32 -0400

    Quit implemented in InputCollector

--- ContactList/main.m
+++ ContactList/main.m
@@ -7,11 +7,25 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "InputCollector.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        InputCollector *inputCollector = [[InputCollector alloc]init];
+        
+        
+        while (YES) {
+            
+             NSString *userResponse = [inputCollector inputForPrompt:@"Welcome to Contact List: What would you like do next? choose one option below\nnew - Create a new contact\nlist - List all contacts\nquit - Exit Application"];
+            
+            if ([userResponse isEqualToString:@"quit"]) {
+                NSLog(@"Good bye!!!");
+                break;
+                
+            }
+        }
+        
+        
     }
     return 0;
 }
